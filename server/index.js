d94eaa44bfbe5d088628421d1e69483a26c224c2
Samuel Muli <samuelmuli95@gmail.com>
2023-12-21 18:34:25 +0300

    Initialize post routes

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ import path from "path"
 import { fileURLToPath } from "url"
 import authRoutes from "./routes/auth"
 import userRoutes from "./routes/users"
+import postRoutes from "./routes/posts"
 import { register } from "./controllers/auth"
+import { createPost } from "./controllers/posts"
+import { verifyToken } from "./middleware/auth"
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url)
@@ -42,10 +45,12 @@ const upload = multer({ storage })
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register)
+app.post("/posts", verifyToken, upload.single("picture"), createPost)
 
 // ROUTES
 app.use("/auth", authRoutes)
 app.use("/users", userRoutes)
+app.use("/posts", postRoutes)
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001
